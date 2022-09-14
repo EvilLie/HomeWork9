6872ed96976dfcf4d948fc9225fd567a8fa55288
Artemiy Sadriev <mosin1898@mail.ru>
2022-09-14 19:29:39 +0300

    Add project files.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,52 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace TelegramBotExperiments
+{
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("5494523799:AAHpDGcbaHLidsTy2UNkv9OYM6EjGEEeJOU");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Hello!");
+                    return;
+                }
+                await botClient.SendTextMessageAsync(message.Chat.Id, $"You said:{message.Text}");
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
